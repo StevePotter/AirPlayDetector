93287d9239b1715dec1af95939a0526a71d00a2e
Stephen Potter <me@stevepotter.me>
2012-05-14 11:16:40 -0400

    First commit

---
+++ AirPlayDetector.h
@@ -0,0 +1,52 @@
+//
+//  AirPlayDetector.h
+//  VEVO HD
+//
+//  Created by Steve Potter on 5/11/12.
+//  Copyright (c) 2012 VEVO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MediaPlayer/MPMoviePlayerController.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+/*
+ * Notification made when [AirPlayDetector isAirPlayAvailable] changes
+ */
+extern NSString *AirPlayAvailabilityChanged;
+
+/*
+ * Does the job of detecting whether this device can connect to an Apple TV or whatever using AirPlay.  It uses a hack with MPVolumeButton to overcome the fact that the framework has no properties or events for this information, which seems pretty silly to me.  
+ 
+ To use it, simply put the following code in your app, typically in appdelegate during applicationDidFinishLaunchingWithOptions:
+ [[AirPlayDetector defaultDetector] startMonitoring:window];
+ 
+ A notification will be posted when availability changes: AirPlayAvailabilityChanged
+ 
+ Or you can query availability by: [AirPlayDetector defaultDetector].isAirPlayAvailable
+ 
+ */
+@interface AirPlayDetector : NSObject 
+{
+@private
+    MPVolumeView *volumeButton;
+    UIView *airplayButton;
+    BOOL isAirPlayAvailable;
+}
+
+/*
+ * When true, there are available airplay devices to play to.
+ */
+@property (readonly, nonatomic) BOOL isAirPlayAvailable;
+
+/*
+ * A convenient static instance of the detector, since apps will almost always use a single shared instance.
+ */
++ (AirPlayDetector*)defaultDetector;
+
+/*
+ * Begins the process of monitoring for changes in airplay availability.
+ */
+- (void)startMonitoring:(UIWindow*)window;
+
+@end
---
+++ AirPlayDetector.m
@@ -0,0 +1,72 @@
+//
+//  AirPlayDetector.m
+//  VEVO HD
+//
+//  Created by Steve Potter on 5/11/12.
+//  Copyright (c) 2012 VEVO. All rights reserved.
+//
+
+#import "AirPlayDetector.h"
+#import <Foundation/Foundation.h>
+#import <MediaPlayer/MPMoviePlayerController.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+NSString *AirPlayAvailabilityChanged = @"AirPlayAvailabilityChanged";
+
+@implementation AirPlayDetector
+
++ (AirPlayDetector *)defaultDetector
+{
+	static AirPlayDetector *defaultDetector;
+	
+	@synchronized(self)
+	{
+		if (!defaultDetector)
+			defaultDetector = [[AirPlayDetector alloc] init];
+		return defaultDetector;
+	}
+}
+
+- (BOOL)isAirPlayAvailable
+{
+    return isAirPlayAvailable;
+}
+
+- (void)startMonitoring:(UIWindow*)window;
+{
+    //here is the real trick.  place an MPVolumeView in the window and monitor for changes in the airplay button's alpha property.  note that this depends on the MPVolumeView's view hierarchy so it must be tested for each iOS release
+    //this was made possible by the awesome sample from http://stackoverflow.com/questions/5388884/airplay-button-on-custom-view-problems
+    
+    volumeButton = [[MPVolumeView alloc] initWithFrame:CGRectMake(-1000,-1000,100,100)];
+    volumeButton.showsVolumeSlider = NO;
+    volumeButton.showsRouteButton = YES;
+    [window addSubview:volumeButton];//if you don't add to a window, nothing will ever happen
+    for (UIView *view in volumeButton.subviews) {
+        if ([view isKindOfClass:[UIButton class]]) {
+            airplayButton = [view retain];
+            isAirPlayAvailable = view.alpha == 1;//it was always 1 in testing but why not, right?
+            [airplayButton addObserver:self forKeyPath:@"alpha" options:NSKeyValueObservingOptionNew context:nil];
+        }
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (![object isKindOfClass:[UIButton class]])
+        return;
+    BOOL isNowAvailable = [[change valueForKey:NSKeyValueChangeNewKey] floatValue] == 1;
+    if ( isNowAvailable != isAirPlayAvailable )
+    {
+        isAirPlayAvailable = isNowAvailable;
+        [[NSNotificationCenter defaultCenter] postNotificationName:AirPlayAvailabilityChanged object:self];
+    }
+}
+
+- (void)dealloc
+{
+    [airplayButton removeObserver:self forKeyPath:@"alpha"];
+    [airplayButton release];
+    [volumeButton release];
+    [super dealloc];
+}
+
+@end
